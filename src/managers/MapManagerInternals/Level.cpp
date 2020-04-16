1b826e80eba814ce5f1542a856c81eea71fba76b
Ege Emir Özkan <egeemirozkan24@gmail.com>
2020-04-16 17:27:38 +0300

    Add infrastructure for level loading

--- src/managers/MapManagerInternals/Level.cpp
+++ src/managers/MapManagerInternals/Level.cpp
@@ -2,11 +2,38 @@
 // Created by elsa on 14.04.2020.
 //
 
+#include <langinfo.h>
 #include "Level.h"
 
+Level* Level::activeLevel = nullptr;
+Level* Level::levels = nullptr;
 
-void Level::importLevelBase(std::string fileName) {
+
+/**
+ * Given the filename of a level, return the level's name, because C++ stdlib is lacking.
+ * @param fileName filename of the level.
+ * @return the level name.
+ */
+std::string parseLevelName(std::string fileName) {
+    int dotIndex;
+    for (int i = 0; i < fileName.length(); i++) {
+        if (fileName[i] == '.') {
+            dotIndex = i;
+            break;
+        }
+    }
+    std::string token = "";
+    std::string levelName = "";
+    for (int i = 0; i <= dotIndex; i++) {
+        levelName += fileName[i];
+    }
+    return levelName;
+}
+
+Level Level::importLevelBase(std::string fileName) {
     FILE *file_ptr;
+    Level* newLevel {};
+    newLevel-> availableZones = Zone::importZones(parseLevelName(fileName));
     file_ptr = fopen(fileName.c_str(), "r");
     int row = 0;
     int column = 0;
@@ -23,11 +50,12 @@ void Level::importLevelBase(std::string fileName) {
                 continue;
             default:
                 const char toIndex = token; // This is a terrible idea use negation instead.
-                this->zoneMatrix[row][column] = availableZones[std::atoi(&toIndex)]; // Seriously fix this before prod.
+                newLevel->zoneMatrix[row][column] = newLevel->availableZones[std::atoi(&toIndex)]; // Seriously fix this before prod.
                 column++;
         }
     }
     fclose(file_ptr);
+    return *newLevel;
 }
 
 void Level::generateTiles() {
@@ -36,4 +64,18 @@ void Level::generateTiles() {
             this->tileMatrix[i][j] = this->zoneMatrix[i][j].generateTile();
         }
     }
+}
+
+void Level::importLevels(std::vector<std::string> mapNames) {
+    levels = (Level*) malloc(sizeof(Level) * mapNames.size());
+    for (auto fileName : mapNames) {
+        *levels = importLevelBase(fileName);
+        levels++;
+    }
+    levels -= mapNames.size();
+    activeLevel = levels;
+}
+
+void Level::changeLevel(int levelIndex) {
+    activeLevel = &levels[levelIndex];
 }
--- src/managers/MapManagerInternals/Level.h
+++ src/managers/MapManagerInternals/Level.h
@@ -6,17 +6,31 @@
 #define TREFUSISENGINE_LEVEL_H
 
 
+#include <vector>
 #include "../../objects/EnviromentalActor.h"
 #include "Zone.h"
 
 class Level {
 private:
     Zone zoneMatrix[500][500];
-    Zone availableZones[5];
-    void importLevelBase(std::string fileName);
+    static Level* levels;
+    Zone* availableZones;
+    /**
+     * Import the base levelmap.
+     * @param fileName File of the level.
+     * @return the level map.
+     */
+    static Level importLevelBase(std::string fileName);
     void generateTiles();
 public:
     EnviromentalActor tileMatrix[500][500];
+    static Level* activeLevel;
+    /**
+     * Given a vector of level names, import the levels.
+     * @param mapNames Names of the maps.
+     */
+    static void importLevels(std::vector<std::string> mapNames);
+    static void changeLevel(int levelIndex);
 };
 
 
