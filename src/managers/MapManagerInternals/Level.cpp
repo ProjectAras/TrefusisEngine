b62d3cb96a20c590f0d4b614b8bdc02e49555f88
Ege Emir Özkan <egeemirozkan24@gmail.com>
2020-04-28 02:35:09 +0300

    Brought level up to par with the previous implementation, plus foilage generation.

--- src/managers/MapManagerInternals/Level.cpp
+++ src/managers/MapManagerInternals/Level.cpp
@@ -12,6 +12,53 @@
 #endif
 Level Level::activeLevel {};
 
+inline envActor copyEnvActor(envActor* ptr) {
+    envActor newEnvActor {ptr->id, ptr->width, ptr->height};
+    return newEnvActor;
+}
+
+inline envActor generateEnvActor(std::vector<envActor> refVector, std::vector<double> chanceVector) {
+    double randomNumber = RandomNumberGenerator::random();
+    for (int i = 0; i < refVector.size(); i++) {
+        if (randomNumber < chanceVector[i]) {
+            return refVector[i];
+        }
+    }
+    return envActor{0,0,0};
+}
+
+inline envActor generateFoilage(zoneProbability* zp) {
+    return generateEnvActor(zp->foilages, zp->foilageChances);
+}
+
+inline envActor generateTiles(zoneProbability* zp) {
+    return generateEnvActor(zp->tiles, zp->tileChances);
+}
+
+Level::Level() {
+#ifdef DEBUG
+    std::cout << "Generating level in mode 1...\n";
+#endif
+    this->tileMatrix.resize(500);
+    this->foilageMatrix.resize(500);
+}
+
+Level::Level(Level* lp) {
+#ifdef DEBUG
+    std::cout << "Generating level in mode 2...\n";
+#endif
+    this->tileMatrix.resize(500);
+    this->foilageMatrix.resize(500);
+    for (int i = 0; i < 500; i++) {
+        this->tileMatrix[i].resize(500);
+        this->foilageMatrix.resize(500);
+        for (int j = 0; j < 500; j++) {
+            this->tileMatrix[i][j] = copyEnvActor(&lp->tileMatrix[i][j]);
+            this->foilageMatrix[i][j] = copyEnvActor(&lp->foilageMatrix[i][j]);
+        }
+    }
+}
+
 void Level::importLevelZoneMatrix(levelProbability *lpp) {
     FILE* fptr;
     fptr = fopen((TrefusisConfig::prefix + TrefusisConfig::mapsDirectory + lpp->levelName).c_str(), "r");
@@ -45,9 +92,12 @@ void Level::importLevelProbabilities(levelProbabilities* lp, const char* fileNam
     fptr = fopen(fileName, "r");
     std::string token;
     ReadingMode mode;
-    envActor* currentEv;
+    zoneProbability currentProbability;
+    envActor* currentEnv;
+    int commaDepth = 0;
+    int char_;
     do {
-        int char_ = fgetc(fptr);
+        char_ = fgetc(fptr);
         switch(char_) {
             case '+':
                 mode = LEVEL;
@@ -56,11 +106,9 @@ void Level::importLevelProbabilities(levelProbabilities* lp, const char* fileNam
                 mode = ZONE;
                 break;
             case '*':
-                currentEv = new envActor();
                 mode = TILE;
                 break;
             case '/':
-                currentEv = new envActor();
                 mode = FOILAGE;
                 break;
             case '\n':
@@ -74,18 +122,109 @@ void Level::importLevelProbabilities(levelProbabilities* lp, const char* fileNam
                         break;
                     case ZONE:
                         lp->probabilities.back().zoneCount += 1;
-                        lp->probabilities.back().zoneProbabilities;
+                        lp->probabilities.back().zoneProbabilities.push_back(zoneProbability{});
+                        break;
+                    case TILE: // Token in this case is the tile spawn chance.
+                        currentProbability = lp->probabilities.back().zoneProbabilities.back();
+                        currentProbability.tileChances.push_back(std::atof(token.c_str())); // We push that chance to the back.
+                        currentProbability.tileCount++;
+                        currentEnv->width = 1;
+                        currentEnv->height = 1;
+                        currentProbability.tiles.push_back(copyEnvActor(currentEnv));
+                        break;
+                    case FOILAGE:
+                        // Likewise, the last token must be foilage spawn chance.
+                        currentProbability = lp->probabilities.back().zoneProbabilities.back();
+                        currentProbability.foilageChances.push_back(std::atof(token.c_str()));
+                        currentProbability.foilageCount++;
+                        currentProbability.foilages.push_back(copyEnvActor(currentEnv));
+                        break;
+                    default:
+                        std::cout << "ERROR: Blank line atop the levels file, or blank levels file.\n";
+                        break;
                 }
+                token = ""; // Done!
+                commaDepth = 0;
+            case ',':
+                switch (mode) {
+                    case TILE:
+                        if (commaDepth > 0) {
+                            std::cout << "ERROR: Too many arguments (or commas) for tile definition.";
+                            exit(1);
+                        } else {
+                            currentEnv->id = std::atoi(token.c_str());
+                        }
+                        break;
+                    case FOILAGE:
+                        switch (commaDepth) {
+                            case 0:
+                                currentEnv->id = std::atoi(token.c_str());
+                                break;
+                            case 1:
+                                currentEnv->width = std::atoi(token.c_str());
+                                break;
+                            case 2:
+                                currentEnv->height = std::atoi(token.c_str());
+                                break;
+                            default:
+                                std::cout << "ERROR: Too many arguments (or commas) for foilage definition.";
+                                exit(1);
+                        }
+                        break;
+                    default:
+                        std::cout << "ERROR: Comma cannot be used outside tile and foilage definitons.";
+                        exit(1);
+                }
+                commaDepth++;
+                token = "";
             default:
                 token += char_;
                 break;
         }
-    }
+    } while(char_ != EOF);
+    free(currentEnv);
 }
 
+void Level::generateEnviromentalActors(levelProbabilities *probabilities) {
+    Level* newLevel {};
+    levelProbability currentLevel;
+    int currentZoneIndex; // Zone Index for this tile.
+    zoneProbability currentZoneProbability; // Current zone probability for this tile.
+    for (int x = 0; x < probabilities->levelCount; x++) {
+        currentLevel = probabilities->probabilities[x];
+        for (int i = 0; i < 500; i++) {
+            for (int j = 0; j < 500; j++) {
+                currentZoneIndex = currentLevel.zoneMatrix[i][j];
+                currentZoneProbability = currentLevel.zoneProbabilities[currentZoneIndex];
+                newLevel->foilageMatrix[i][j] = generateFoilage(&currentZoneProbability);
+                newLevel->tileMatrix[i][j] = generateTile(&currentZoneProbability);
+            }
+        }
+        levels.push_back(new Level(newLevel));
+    }
+    free(newLevel);
+}
+
+void Level::smoothTiles() {
+    std::vector<envActor> surrounding;
+    for (Level level : levels) {
+        for (int i = 0; i < 500; i++) {
+            for (int j = 0; j < 500; j++) {
+                surrounding.push_back(level.tileMatrix[i - 1][j]);
+                surrounding.push_back(level.tileMatrix[i][j - 1]);
+                surrounding.push_back(level.tileMatrix[i + 1][j]);
+                surrounding.push_back(level.tileMatrix[i][j + 1]);
+                level.tileMatrix[i][j] = surrounding[RandomNumberGenerator::randint(0, 4)];
+                surrounding.clear();
+            }
+        }
+    }
+}
 levelProbabilities Level::importLevels() {
     levelProbabilities lp;
     importLevelProbabilities(&lp, TrefusisConfig::levelsLocation.c_str());
+    generateEnviromentalActors(&lp);
+    smoothTiles();
     activeLevel = levels[0];
 }
 
@@ -93,8 +232,3 @@ void Level::changeLevel(int levelIndex) {
     activeLevel = levels[levelIndex];
 }
 
-Level::Level() {
-#ifdef DEBUG
-    std::cout << "Generating level...\n";
-#endif
-}
--- src/managers/MapManagerInternals/Level.h
+++ src/managers/MapManagerInternals/Level.h
@@ -7,16 +7,24 @@
 
 
 #include <vector>
-#include "../../objects/EnviromentalActor.h"
 
+/**
+ * Represents the reading mode the parser is in.
+ */
 enum ReadingMode {LEVEL, ZONE, TILE, FOILAGE};
 
+/**
+ * Enviromental actor structure.
+ */
 struct envActor {
     int id;
     int width;
     int height;
 };
 
+/**
+ * Holds the spawn probability rule objects for one zone.
+ */
 struct zoneProbability {
     int tileCount;
     int foilageCount;
@@ -26,6 +34,10 @@ struct zoneProbability {
     std::vector<envActor> foilages;
 };
 
+
+/**
+ * Holds the spawn probability rule objects for one level.
+ */
 struct levelProbability {
     const char* levelName;
     std::vector<std::vector<int>> zoneMatrix;
@@ -33,34 +45,79 @@ struct levelProbability {
     std::vector<zoneProbability> zoneProbabilities;
 };
 
+/**
+ * Holds the probability objects for all levels.
+ */
 struct levelProbabilities {
     int levelCount;
     std::vector<std::string> levelNames;
     std::vector<levelProbability> probabilities;
 };
 
+/**
+ * Copy an enviromental actor instance and return its copy. This may not be necessary, however I have no time to test
+ * So I am being defensive.
+ * @param ptr Pointer to the object.
+ * @return Its clone.
+ */
+inline envActor copyEnvActor(envActor* ptr);
 
+/**
+ * Generate a tile given zone probability object.
+ * @param zp Zone probability object containing rules to generate the tile.
+ * @return The generated tile.
+ */
+inline envActor generateTile(zoneProbability* zp);
+
+/**
+ * Generate foilage given zone probability object.
+ * @param zp Zone probability object to generate foilage for.
+ * @return the generated foilage object.
+ */
+inline envActor generateFoilage(zoneProbability* zp);
+
+/**
+ * Generate an enviromental actor object given
+ * @param referanceVector The vector holding possible enviromental objects.
+ * @param probabilityVector Probability vector holding their spawn chance.
+ * @return
+ */
+inline envActor generateEnvActor(std::vector<envActor> referanceVector, std::vector<double> probabilityVector);
 class Level {
 private:
-    Level();
-    std::vector<std::vector<EnviromentalActor>> foilageMatrix;
-    static std::vector<Level> levels;
-
-
-    static void importLevelProbabilities(levelProbabilities* lp, const char* fileName);
-
     /**
-     * Import the base levelmap.
-     * @param fileName File of the level.
-     * @return the level map.
+     * Normal constructor for level.
      */
-    static void importLevelBase(levelProbabilities probabilities);
+    Level();
+    /**
+     * Copy constructor.
+     * @param lp
+     */
+    Level(Level* lp);
+    static std::vector<Level> levels; // Holds all the levels.
+    /**
+     * Import the spawn probabilities for levels.
+     * @param lp Level probabilities object.
+     * @param fileName Name of the file.
+     */
+    static void importLevelProbabilities(levelProbabilities* lp, const char* fileName);
+    /**
+     * Import the zone matrix for a level.
+     * @param lpp Level probability pointer to import to.
+     */
     static void importLevelZoneMatrix(levelProbability* lpp);
-    void generateTiles();
-    void smoothTiles();
-    void generateFoilages();
+    /**
+     * Generate the enviromental actors for all levels.
+     * @param probabilities Probabilities container for all levels.
+     */
+    static void generateEnviromentalActors(levelProbabilities* probabilities);
+    /**
+     * Smoothen the tiles for all levels. RIP Conway.
+     */
+    static void smoothTiles();
 public:
-    std::vector<std::vector<EnviromentalActor>> tileMatrix;
+    std::vector<std::vector<envActor>> foilageMatrix;
+    std::vector<std::vector<envActor>> tileMatrix;
     static Level activeLevel;
     /**
      * Given a vector of level names, import the levels.
